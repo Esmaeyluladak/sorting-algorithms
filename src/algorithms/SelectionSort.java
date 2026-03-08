1cb750a5a2ac50fde057a7fd00ef7e1da2ba9947
Asli Oztasci <aslioztasci@gmail.com>
2026-03-08 14:39:30 +0300

    Add SelectionSort algorithm implementation

---
+++ src/algorithms/SelectionSort.java
@@ -0,0 +1,45 @@
+public class SelectionSort {
+
+public static int[] sort(int[] arr) {
+
+int n = arr.length;
+
+for (int i = 0; i < n - 1; i++) {
+
+int minIdx = i;
+
+for (int j = i + 1; j < n; j++) {
+
+if (arr[j] < arr[minIdx]) minIdx = j;
+
+}
+
+int temp = arr[minIdx];
+
+arr[minIdx] = arr[i];
+
+arr[i] = temp;
+
+}
+
+return arr;
+
+}
+
+public static void main(String[] args) {
+
+int[] arr = {64, 25, 12, 22, 11};
+
+System.out.println("Before sorting:");
+
+for (int num : arr) System.out.print(num + " ");
+
+sort(arr);
+
+System.out.println("\nAfter sorting:");
+
+for (int num : arr) System.out.print(num + " ");
+
+}
+
+}
